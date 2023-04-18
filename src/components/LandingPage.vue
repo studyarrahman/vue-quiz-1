e43935f638814f093ed501026b5d8f2a9499527c
NOVRI RAHMAN <129195286+studyarrahman@users.noreply.github.com>
2023-04-18 15:09:18 +0700

    Update LandingPage.vue

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <router-link class="nav-link" to="/home">Home</router-link>
+            <router-link class="nav-link" to="/">Home</router-link>
         </li>
         <li class="nav-item">
             <router-link class="nav-link" to="/portfolio">Portfolio</router-link>
